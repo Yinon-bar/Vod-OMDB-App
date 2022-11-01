602f9917ee0d13b9e666df228ec4a08b53a4081f
Yinon Bar <yinonbar1988@gmail.com>
2022-11-01 14:57:20 +0200

    first commit

--- src/Components/VodInput/VodInput.js
+++ src/Components/VodInput/VodInput.js
@@ -1,9 +1,20 @@
+import "./VodInput.css";
+
 function VodInput() {
   return (
-    <div className="VodInput">
-      <h3>VodInput</h3>
-      {/*  */}
-    </div>
+    <header className="VodInput container-fluid bg-dark p-2">
+      <div className="container">
+        <div className="row">
+          <div className="logo col-auto">
+            <h3 className="text-white">VodInput</h3>
+          </div>
+          <nav className="d-flex col-md-5">
+            <input type="search" className="form-control" />
+            <button className="btn btn-warning">Search</button>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 }
 
